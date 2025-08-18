8c950fa3583966b1ddee20f638b61101d2903d29
Igor Kostyuchenok <igor.kostyuchenok@gmail.com>
2025-08-18 10:49:38 +0200

    GTM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter  as Router, Routes, Route } from 'react-router-dom'
+import TagManager from 'react-gtm-module';
 import App from './App'
 import ContactPage from './ContactPage'
 import InvestorsPage from './InvestorsPage'
 import './styles/index.css'
+
+const tagManagerArgs = {
+  gtmId: 'G-PDMD7RMPEL',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
